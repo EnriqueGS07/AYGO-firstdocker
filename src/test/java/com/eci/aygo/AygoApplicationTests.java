90fc43424ee9ab9d420dab63697339c892f7f691
Enrique González <98104282+EnriqueGS07@users.noreply.github.com>
2025-10-26 00:15:04 -0500

    Aygo taller 1

---
+++ src/test/java/com/eci/aygo/AygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.aygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AygoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
