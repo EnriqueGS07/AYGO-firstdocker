dd2f2b46aaccbcaaa9dd7ba693ee2f01c2a0bc05
Enrique González <98104282+EnriqueGS07@users.noreply.github.com>
2025-10-26 00:13:28 -0500

    Aygo firstdockercontainer

---
+++ aygo/src/main/java/com/eci/aygo/AygoApplication.java
@@ -0,0 +1,13 @@
+package com.eci.aygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AygoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AygoApplication.class, args);
+	}
+
+}
